2abc5f0dfda878fdee1340cbc8cbf7f600db761b
bijan <wsolstice12@gmail.com>
2017-07-04 17:42:45 -0700

    first commit

--- web-app/app/index.js
+++ web-app/app/index.js
@@ -1,16 +1,16 @@
 import * as d3 from 'd3';
 import bar from 'britecharts/dist/umd/bar.min';
-import * as britecharts from "britecharts";
+import * as britecharts from 'britecharts';
 
-import 'britecharts/dist/css/britecharts.min.css'
+import 'britecharts/dist/css/britecharts.min.css';
 
 const data = [
-  {name: 'Shiny', id: 1, quantity: 86, percentage: 5},
-  {name: 'Blazing', id: 2, quantity: 300, percentage: 18},
-  {name: 'Dazzling', id: 3, quantity: 276, percentage: 16},
-  {name: 'Radiant', id: 4, quantity: 195, percentage: 11},
-  {name: 'Sparkling', id: 5, quantity: 36, percentage: 2},
-  {name: 'Other', id: 0, quantity: 814, percentage: 48}
+  { name: 'Shiny', id: 1, quantity: 86, percentage: 5 },
+  { name: 'Blazing', id: 2, quantity: 300, percentage: 18 },
+  { name: 'Dazzling', id: 3, quantity: 276, percentage: 16 },
+  { name: 'Radiant', id: 4, quantity: 195, percentage: 11 },
+  { name: 'Sparkling', id: 5, quantity: 36, percentage: 2 },
+  { name: 'Other', id: 0, quantity: 814, percentage: 48 },
 ];
 
 function createHorizontalBarChart(tag) {
@@ -19,7 +19,7 @@ function createHorizontalBarChart(tag) {
     left: 120,
     right: 20,
     top: 20,
-    bottom: 30
+    bottom: 30,
   };
   const barContainer = d3.select(tag);
   const containerWidth = barContainer.node() ? barContainer.node().getBoundingClientRect().width : false;
